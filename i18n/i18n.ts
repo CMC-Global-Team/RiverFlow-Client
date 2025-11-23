7ef062f860a6a440574ec016eb1f73f42ccb845e
tuong1901 <hoanghoangcattuong@gmail.com>
2025-11-23 23:03:50 +0700

    Update translation for file i18n.ts

--- i18n/i18n.ts
+++ i18n/i18n.ts
@@ -3,8 +3,12 @@ import i18n from "i18next";
 import { initReactI18next } from "react-i18next";
 import HOME_EN from "@/locales/en/home.json"
 import HOME_VI from "@/locales/vi/home.json"
+import ABOUT_EN from "@/locales/en/about.json"
+import ABOUT_VI from "@/locales/vi/about.json"
+import PRICING_EN from "@/locales/en/pricing.json"
+import PRICING_VI from "@/locales/vi/pricing.json"
 
-export  const locale = {
+export const locale = {
     en: "English",
     vi: "Tiếng Việt"
 } as const
@@ -12,17 +16,21 @@ export  const locale = {
 
 const resource = {
     en: {
-        home: HOME_EN
+        home: HOME_EN,
+        about: ABOUT_EN,
+        pricing: PRICING_EN
     },
-    vi:  {
-        home: HOME_VI
+    vi: {
+        home: HOME_VI,
+        about: ABOUT_VI,
+        pricing: PRICING_VI
     }
 } as const
 const defaultNS = 'home'
 i18n.use(initReactI18next).init({
     resources: resource,
     lng: "vi",
-    ns: ["home"],
+    ns: ["home", "about", "pricing"],
     defaultNS,
     fallbackLng: "vi",
     interpolation: {
